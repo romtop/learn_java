b874d4c9cbdd4315fde4b30548243418fec1e6c0
romarik75 <roman.topka@gmail.com>
2018-07-12 19:42:15 +0300

    Learning Functions

--- sandbox/src/main/java/learn/java/MyFirstProgram.java
+++ sandbox/src/main/java/learn/java/MyFirstProgram.java
@@ -4,7 +4,31 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
 
-        System.out.println("learn.java.MyFirstProgram");
+       /*hello("world");
+       hello("user");
+       hello("Roman");*/
+
+       //double len = 5;
+       //area(5);
+       //area(8);
+
+       double a = 3;
+       double b = 4;
+       System.out.println("Площадь прямоугольника со сторонами " + a + ", " + b + " = " + area(a, b));
+
     }
 
+    /*public static void hello (String s) {
+
+        System.out.println("Hello, " + s + "!");
+    }*/
+    //public static double area(double l) {
+        //System.out.println("Площадь квадрата со стороной " + l + " = " + l * l);
+        //return l * l;
+   // }
+
+    public static double area (double a, double b) {
+        return a * b;
+
+    }
 }
