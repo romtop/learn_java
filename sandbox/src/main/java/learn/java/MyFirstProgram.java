e05b44fbc37f48ebdfee19330150f0239a12deae
romarik75 <roman.topka@gmail.com>
2018-07-12 13:39:47 +0300

    Formatting Settings were changed

--- sandbox/src/main/java/learn/java/MyFirstProgram.java
+++ sandbox/src/main/java/learn/java/MyFirstProgram.java
@@ -2,9 +2,9 @@ package learn.java;
 
 public class MyFirstProgram {
 
-	public static void main(String[] args) {
-			
-	System.out.println ("learn.java.MyFirstProgram");
-	}
+    public static void main(String[] args) {
+
+        System.out.println("learn.java.MyFirstProgram");
+    }
 
 }
