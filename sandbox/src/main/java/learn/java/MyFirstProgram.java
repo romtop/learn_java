7ab374b6d57de1fd985a7b949efe2e5eefa21e64
romarik75 <roman.topka@gmail.com>
2018-07-15 21:01:55 +0300

    Fighthing with step 4 (homework 2)

--- sandbox/src/main/java/learn/java/MyFirstProgram.java
+++ sandbox/src/main/java/learn/java/MyFirstProgram.java
@@ -4,9 +4,9 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
 
-        Point p1 = new Point (4, 6); // есть
+        Point p1 = new Point (4, 6);
 
-        Point p2 = new Point (2, 3); // есть
+        Point p2 = new Point (2, 3);
 
         System.out.println ("Расстояние между точкой A с координатами " + p1.x1 + ", " + p1.x2 + " и точкой В с координатами " + p2.y1 + ", " + p2.y2 + " = " + distance ());
 
@@ -18,8 +18,5 @@ public class MyFirstProgram {
        System.out.println("Сумма сторон прямоугольника, составлящих " + s.c + " и " + s.d + ", равна " + s.area());*/
 
     }
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt ((x1-x2) * (x1-x2) + (y1-y2) * (y1-y2));
-    }
 
-}
+    }
--- sandbox/src/main/java/learn/java/Point.java
+++ sandbox/src/main/java/learn/java/Point.java
@@ -7,14 +7,17 @@ public class Point {
    public double x2;
    public double y2;
 
-   public Point (double x1, double y1) {
+   public Point(double x1, double y1) {
       this.x1 = x1;
       this.y1 = y1;
    }
 
-   public Point (double x2, double y2) {
+   public Point(double x2, double y2) {
       this.x2 = x2;
       this.y2 = y2;
    }
 
+   public double distance() {
+      return Math.sqrt((this.x1 - this.x2) * (this.x1 - this.x2) + (this.y1 - this.y2) * (this.y1 - this.y2));
    }
+}
