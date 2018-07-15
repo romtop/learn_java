2af2641ffa6c6909efd3da4e679cb3a949c7665b
romarik75 <roman.topka@gmail.com>
2018-07-15 19:59:36 +0300

    Struggle with 1-3 steps (homework 2)

--- sandbox/src/main/java/learn/java/MyFirstProgram.java
+++ sandbox/src/main/java/learn/java/MyFirstProgram.java
@@ -4,12 +4,23 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
 
+        Point p1 = new Point (4, 6); // есть
+
+        Point p2 = new Point (2, 3); // есть
+
+        public static double distance(Point p1, Point p2) {
+            Math.sqrt ((x1-x2) * (x1-x2) + (y1-y2) * (y1-y2));
+        }
+
+        System.out.println ("Расстояние между точкой A с координатами " + p1.x1 + ", " + p1.x2 + " и точкой В с координатами " + p2.y1 + ", " + p2.y2 + " = " + distance ());
+
+
+       /*System.out.println("Расстояние между точками " + p1 + " и " + p2 + " = " + Point p1, Point p2);
+
        Area r = new Area(3, 4);
-
        Sum s = new Sum(2, 6);
-
        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
-       System.out.println("Сумма сторон прямоугольника, составлящих " + s.c + " и " + s.d + ", равна " + s.area());
+       System.out.println("Сумма сторон прямоугольника, составлящих " + s.c + " и " + s.d + ", равна " + s.area());*/
 
     }
 
