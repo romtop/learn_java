bfd1de4a0ca34227e287ef820cbd39bad37822a4
romarik75 <roman.topka@gmail.com>
2018-07-16 18:28:28 +0300

    step 4

--- sandbox/src/main/java/learn/java/Point.java
+++ sandbox/src/main/java/learn/java/Point.java
@@ -7,12 +7,9 @@ public class Point {
    public double x2;
    public double y2;
 
-   public Point(double x1, double y1) {
+   public Point(double x1, double y1, double x2, double y2) {
       this.x1 = x1;
       this.y1 = y1;
-   }
-
-   public Point(double x2, double y2) {
       this.x2 = x2;
       this.y2 = y2;
    }
