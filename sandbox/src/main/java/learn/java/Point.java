4f387d685c1edd3caa0af8b97e0c40839b4dd911
romarik75 <roman.topka@gmail.com>
2018-07-22 20:07:42 +0300

    Homework 3, p.2 - Tests for Methods

--- sandbox/src/main/java/learn/java/Point.java
+++ sandbox/src/main/java/learn/java/Point.java
@@ -11,7 +11,7 @@ public class Point {
       this.y = y;
    }
 
-   //public double distance(Point p2) { // method
-     // return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
-  // }
+   public double distance(Point p2) {
+     return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
+   }
 }
--- sandbox/src/test/java/learn/java/DistanceTests.java
+++ sandbox/src/test/java/learn/java/DistanceTests.java
@@ -3,21 +3,26 @@ package learn.java;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static learn.java.MyFirstProgram.distance;
-
 public class DistanceTests {
 
-   @Test // 1. Test for function, positive quadrants
+   @Test // 1. Test for method, common case
       public void testDistance1 () {
       Point p1 = new Point(4, 6);
       Point p2 = new Point(2, 3);
-      Assert.assertEquals (distance( p1, p2), 3.605551275463989);
+      assert (p1.distance(p2) == 3.605551275463989);
    }
 
-   @Test // 2. Test for function, positive and negative quadrants
+   @Test // 2. Test for method, positive quadrants
    public void testDistance2 () {
+      Point p1 = new Point(4, 6);
+      Point p2 = new Point(2, 3);
+      Assert.assertEquals (p1.distance(p2), 3.605551275463989);
+   }
+
+   @Test // 2. Test for method, positive and negative quadrants
+   public void testDistance3 () {
       Point p1 = new Point(-4, 6);
       Point p2 = new Point(2, -3);
-      Assert.assertEquals (distance( p1, p2), 10.816653826391969);
+      Assert.assertEquals (p1.distance(p2), 10.816653826391969);
    }
 }
