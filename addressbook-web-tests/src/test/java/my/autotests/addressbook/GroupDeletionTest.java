e361cde3aad8ca243d6f661069dce08894960bcc
romarik75 <roman.topka@gmail.com>
2018-07-30 22:27:47 +0300

    GroupDeletionTest - Pulling Up to Test Base

---
+++ addressbook-web-tests/src/test/java/my/autotests/addressbook/GroupDeletionTest.java
@@ -0,0 +1,16 @@
+package my.autotests.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTest extends TestBase {
+
+    @Test
+    public void testGroupDeletion() {
+
+        gotoGroupPage();
+        selectGroup();
+        deleteSelectedGroups();
+        returnToGroupPage();
+    }
+
+}
