ad6eb2e458f515dc1c9cccbf7a28c048305fbebf
romarik75 <roman.topka@gmail.com>
2018-08-04 22:05:03 +0300

    Groups: Creation of Helper classes

--- addressbook-web-tests/src/test/java/my/autotests/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/my/autotests/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,6 @@
 package my.autotests.addressbook.appmanager;
 
 import my.autotests.addressbook.model.ContactData;
-import my.autotests.addressbook.model.GroupData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,6 +10,10 @@ import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
    FirefoxDriver wd;
+
+   private SessionHelper sessionHelper;
+   private NavigationHelper navigationHelper;
+   private GroupHelper groupHelper;
 
    public static boolean isAlertPresent(FirefoxDriver wd) {
        try {
@@ -25,59 +28,14 @@ public class ApplicationManager {
       wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
       wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
       wd.get("http://localhost/addressbook/");
-      login("admin", "secret");
-   }
-
-   private void login(String username, String password) {
-       wd.findElement(By.name("pass")).click();
-       wd.findElement(By.name("pass")).clear();
-       wd.findElement(By.name("pass")).sendKeys(password);
-       wd.findElement(By.name("user")).click();
-       wd.findElement(By.name("user")).clear();
-       wd.findElement(By.name("user")).sendKeys(username);
-       wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-   }
-
-   public void returnToGroupPage() {
-       wd.findElement(By.linkText("group page")).click();
-   }
-
-   public void submitGroupCreation() {
-       wd.findElement(By.name("submit")).click();
-   }
-
-   public void fillGroupForm(GroupData groupData) {
-       wd.findElement(By.name("group_name")).click();
-       wd.findElement(By.name("group_name")).clear();
-       wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-       wd.findElement(By.name("group_header")).click();
-       wd.findElement(By.name("group_header")).clear();
-       wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-       wd.findElement(By.name("group_footer")).click();
-       wd.findElement(By.name("group_footer")).clear();
-       wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-   }
-
-   public void initGroupCreation() {
-       wd.findElement(By.name("new")).click();
-   }
-
-   public void gotoGroupPage() {
-       wd.findElement(By.linkText("groups")).click();
+      groupHelper = new GroupHelper(wd);
+      navigationHelper = new NavigationHelper(wd);
+      sessionHelper = new SessionHelper(wd);
+      sessionHelper.login("admin", "secret");
    }
 
    public void stop() {
       wd.quit();
-   }
-
-   public void deleteSelectedGroups() {
-       wd.findElement(By.name("delete")).click();
-   }
-
-   public void selectGroup() {
-       if (!wd.findElement(By.name("selected[]")).isSelected()) {
-           wd.findElement(By.name("selected[]")).click();
-       }
    }
 
    public void returnToHomePage() {
@@ -122,5 +80,13 @@ public class ApplicationManager {
       if (!wd.findElement(By.name("selected[]")).isSelected()) {
          wd.findElement(By.name("selected[]")).click();
       }
+   }
+
+   public GroupHelper getGroupHelper() {
+      return groupHelper;
+   }
+
+   public NavigationHelper getNavigationHelper() {
+      return navigationHelper;
    }
 }
--- addressbook-web-tests/src/test/java/my/autotests/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/my/autotests/addressbook/tests/GroupCreationTest.java
@@ -8,11 +8,11 @@ public class GroupCreationTest extends TestBase{
     @Test
     public void testGroupCreation() {
 
-        app.gotoGroupPage();
-        app.initGroupCreation();
-        app.fillGroupForm(new GroupData("Test1", "Test1test1", "Test1"));
-        app.submitGroupCreation();
-        app.returnToGroupPage();
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().initGroupCreation();
+        app.getGroupHelper().fillGroupForm(new GroupData("Test1", "Test1test1", "Test1"));
+        app.getGroupHelper().submitGroupCreation();
+        app.getGroupHelper().returnToGroupPage();
     }
 
 }
